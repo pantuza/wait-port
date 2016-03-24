7246b6be8a8a3f72f4e04a1fbef3f30fbde272f0
Gustavo Pantuza <gustavopantuza@gmail.com>
2016-03-24 13:15:08 -0300

    Created the main file of the wait-port project

---
+++ src/wait_port.c
@@ -0,0 +1,25 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  wait_port.c
+ *
+ *    Description:  Main file of the wait-port project
+ *
+ *        Created:  03/24/2016 13:08:56
+ *
+ *         Author:  Gustavo Pantuza
+ *   Organization:  Software Community
+ *
+ * ============================================================================
+ */
+
+
+#include <stdlib.h>
+
+
+int
+main (int argc, char* argv[])
+{
+
+    return EXIT_SUCCESS;
+}
